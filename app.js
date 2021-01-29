ce96f17f9ca537d807aed76035b084c788ef9fef
DKlaver <dklaver15@gmail.com>
2021-01-28 18:44:42 -0600

    Added an additional rule to the log. Also added a generateRandomNumber function to the Computer class.

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ class Game {
 
 		this.gestures = [];
 
-		this.gestures.push(new Gesture("Rock"));
-		this.gestures.push(new Gesture("Paper"));
-		this.gestures.push(new Gesture("Scissors"));
-		this.gestures.push(new Gesture("Lizard"));
-		this.gestures.push(new Gesture("Spock"));
+		this.gestures.push(new Gesture("rock"));
+		this.gestures.push(new Gesture("paper"));
+		this.gestures.push(new Gesture("scissors"));
+		this.gestures.push(new Gesture("lizard"));
+		this.gestures.push(new Gesture("spock"));
 
 		this.numberOfGames = 0;
 	}
@@ -38,6 +38,7 @@ class Game {
 	displayRules() {
 		console.log("Welcome to Rock-Paper-Scissors-Lizard-Spock!");
 		console.log("Two players will pick a gesture with which to battle their opponent.");
+		console.log("Player 1 may choose to play against another player or the computer.")
 		console.log("If the first player chooses to play against the computer, the computer will choose its gestures at random.");
 		console.log("The match will consist of 3 rounds.");
 		console.log("The first player to win at least 2 rounds is the winner of the match.")
@@ -57,25 +58,33 @@ class Game {
 
 class Player {
 	constructor(name) {
-		this.score = 0;
 		this.name = name;
+		this.score = 0;
 	}
 
-	chooseGesture(gesture) {
-		let choice = prompt("Please select a gesture/item.");
+	chooseGesture(gestureArray) {
+		let choice = prompt("Please select a gesture.");
+		return choice;
 	}
 }
 
 class Human extends Player {
 	constructor(name) {
-
+		super(name);
+		this.score = 0;
 	}
 }
 
 class Computer extends Player {
 	constructor(name) {
-
+		super(name);
+		this.score = 0;
 	}
+
+	generateRandomNumber() {
+		let randomNumber = Math.floor(Math.random() * this.numberOfSides) + 1;
+		return randomNumber;
+	 }
 }
 
 /*====================================================================*/
