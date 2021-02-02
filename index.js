03612901c08e8d3992b773ed62b6a02a16f4675c
DKlaver <dklaver15@gmail.com>
2021-02-01 20:19:15 -0600

    Initial file creation so program can run in Node.js

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//Rock-Paper-Scissors-Lizard-Spock Game in Node.JS
+
+// two player game
+// user chooses to play against either the computer or another player
+// player 1 choose a gesture/item
+// validate input from player 1
+// players 2 or the computer choose a gesture
+// validate input from player 2 (validation not needed if playing against the computer)
+// compare the gestures
+// determine/display the winner
+// repeate 2 more times for a total of "best out of 3"
+
+const prompt = require("prompt-sync")();
+const classes = require("./classes");
+
+let game = new classes.game();
+game.runGame();
